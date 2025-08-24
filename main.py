d5b804dd8e1fea12efbd30d6545f81329ba38ee0
Renan-RodriguesDEV <renanrodrigues7110@gmail.com>
2025-08-24 10:28:06 -0300

    Atualiza créditos no README e adiciona rodapé com autores no app Streamlit

--- main.py
+++ main.py
@@ -172,3 +172,9 @@ Mostre a chave final (menor, porém segura).
 O que falar: “Mesmo com ruído, refinamos a chave final segura.
     """
     )
+
+    st.html("""
+    <div style="position: fixed; bottom: 10px; left: 0; width: 100%; text-align: center; color: #666; font-size:12px;">
+        Created by: Sr. Me. Renan Supremo, Pedro e Mariani
+    </div>
+    """)
